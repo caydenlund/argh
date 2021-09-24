dd5ecc01c848881c90636bf6b1efccb64cfdcddd
shrimpster00 <caydenlund@gmail.com>
2021-09-24 14:42:58 -0600

    Write argh header.

---
+++ src/argh/argh.cc
@@ -0,0 +1,16 @@
+// src/argh/argh.cc
+// v0.1.0
+//
+// Author: Cayden Lund
+//   Date: 09/24/2021
+//
+// This file contains the argh implementation.
+// Use this utility to parse command line arguments.
+//
+// Copyright (C) 2021 Cayden Lund <https://github.com/shrimpster00>
+// License: MIT (opensource.org/licenses/MIT)
+
+#include "argh.h"
+
+namespace argh {
+}
