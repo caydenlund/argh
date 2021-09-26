773560ee4e8f007c1b715ed941943aa7aa03f97d
shrimpster00 <caydenlund@gmail.com>
2021-09-26 06:13:33 -0600

    Update tests to follow the new specifications.

--- src/argh/argh.cc
+++ src/argh/argh.cc
@@ -2,7 +2,7 @@
 // v0.2.0
 //
 // Author: Cayden Lund
-//   Date: 09/24/2021
+//   Date: 09/26/2021
 //
 // This file contains the argh implementation.
 // Use this utility to parse command line arguments.
--- src/argh/tests/argh.test.cc
+++ src/argh/tests/argh.test.cc
@@ -1,8 +1,8 @@
 // src/argh/tests/argh.test.cc
-// v0.1.0
+// v0.2.0
 //
 // Author: Cayden Lund
-//   Date: 09/24/2021
+//   Date: 09/26/2021
 //
 // This file contains the unit tests for the argh library.
 //
@@ -19,28 +19,31 @@
 TEST(argh_argh_test, argh_argh_constructor_test)
 {
     std::string argv_a[] = {"test"};
-    argh::argh args_a(argv_a);
+    argh::argh args_a(1, argv_a);
+
+    size_t memLeakCount;
+    uint64_t memLeakSize;
 
     std::string argv_b[] = {"test", "-h"};
-    argh::argh args_b(argv_b);
+    argh::argh args_b(2, argv_b);
 
     std::string argv_c[] = {"test", "--help"};
-    argh::argh args_c(argv_c);
+    argh::argh args_c(2, argv_c);
 
     std::string argv_d[] = {"test", "-hv"};
-    argh::argh args_d(argv_d);
+    argh::argh args_d(2, argv_d);
 
     std::string argv_e[] = {"test", "--help", "-v"};
-    argh::argh args_e(argv_e);
+    argh::argh args_e(3, argv_e);
 
     std::string argv_f[] = {"test", "-hv", "-o", "output.txt"};
-    argh::argh args_f(argv_f);
+    argh::argh args_f(4, argv_f);
 
     std::string argv_g[] = {"test", "-vo", "output.txt"};
-    argh::argh args_g(argv_g);
+    argh::argh args_g(3, argv_g);
 
     std::string argv_h[] = {"test", "--output=output.txt"};
-    argh::argh args_h(argv_h);
+    argh::argh args_h(2, argv_h);
 }
 
 // Test the argh::argh class operator[] for flags.
@@ -49,7 +52,7 @@ TEST(argh_argh_test, argh_argh_constructor_test)
 TEST(argh_argh_test, argh_argh_operator_flag_test)
 {
     std::string argv_a[] = {"test"};
-    argh::argh args_a(argv_a);
+    argh::argh args_a(1, argv_a);
     ASSERT_FALSE(args_a["-h"]);
     ASSERT_FALSE(args_a["--help"]);
     ASSERT_FALSE(args_a["-v"]);
@@ -57,7 +60,7 @@ TEST(argh_argh_test, argh_argh_operator_flag_test)
     ASSERT_FALSE(args_a["-o"]);
 
     std::string argv_b[] = {"test", "-h"};
-    argh::argh args_b(argv_b);
+    argh::argh args_b(2, argv_b);
     ASSERT_TRUE(args_b["-h"]);
     ASSERT_FALSE(args_b["--help"]);
     ASSERT_FALSE(args_b["-v"]);
@@ -65,7 +68,7 @@ TEST(argh_argh_test, argh_argh_operator_flag_test)
     ASSERT_FALSE(args_b["-o"]);
 
     std::string argv_c[] = {"test", "--help"};
-    argh::argh args_c(argv_c);
+    argh::argh args_c(2, argv_c);
     ASSERT_FALSE(args_c["-h"]);
     ASSERT_TRUE(args_c["--help"]);
     ASSERT_FALSE(args_c["-v"]);
@@ -73,7 +76,7 @@ TEST(argh_argh_test, argh_argh_operator_flag_test)
     ASSERT_FALSE(args_c["-o"]);
 
     std::string argv_d[] = {"test", "-hv"};
-    argh::argh args_d(argv_d);
+    argh::argh args_d(2, argv_d);
     ASSERT_TRUE(args_d["-h"]);
     ASSERT_FALSE(args_d["--help"]);
     ASSERT_TRUE(args_d["-v"]);
@@ -81,7 +84,7 @@ TEST(argh_argh_test, argh_argh_operator_flag_test)
     ASSERT_FALSE(args_d["-o"]);
 
     std::string argv_e[] = {"test", "--help", "-v"};
-    argh::argh args_e(argv_e);
+    argh::argh args_e(3, argv_e);
     ASSERT_FALSE(args_e["-h"]);
     ASSERT_TRUE(args_e["--help"]);
     ASSERT_TRUE(args_e["-v"]);
@@ -89,7 +92,7 @@ TEST(argh_argh_test, argh_argh_operator_flag_test)
     ASSERT_FALSE(args_e["-o"]);
 
     std::string argv_f[] = {"test", "-hv", "-o", "output.txt"};
-    argh::argh args_f(argv_f);
+    argh::argh args_f(4, argv_f);
     ASSERT_TRUE(args_f["-h"]);
     ASSERT_FALSE(args_f["--help"]);
     ASSERT_TRUE(args_f["-v"]);
@@ -97,7 +100,7 @@ TEST(argh_argh_test, argh_argh_operator_flag_test)
     ASSERT_TRUE(args_f["-o"]);
 
     std::string argv_g[] = {"test", "-vo", "output.txt"};
-    argh::argh args_g(argv_g);
+    argh::argh args_g(3, argv_g);
     ASSERT_FALSE(args_g["-h"]);
     ASSERT_FALSE(args_g["--help"]);
     ASSERT_TRUE(args_g["-v"]);
@@ -105,7 +108,7 @@ TEST(argh_argh_test, argh_argh_operator_flag_test)
     ASSERT_TRUE(args_g["-o"]);
 
     std::string argv_h[] = {"test", "--output=output.txt"};
-    argh::argh args_h(argv_h);
+    argh::argh args_h(2, argv_h);
     ASSERT_TRUE(args_h["--output"]);
     ASSERT_FALSE(args_h["--output=output.txt"]);
 }
@@ -118,31 +121,31 @@ TEST(argh_argh_test, argh_argh_operator_flags_test)
     std::string args_list[] = {"-h", "--help"};
 
     std::string argv_a[] = {"test"};
-    argh::argh args_a(argv_a);
+    argh::argh args_a(1, argv_a);
     ASSERT_FALSE(args_a[args_list]);
 
     std::string argv_b[] = {"test", "-h"};
-    argh::argh args_b(argv_b);
+    argh::argh args_b(2, argv_b);
     ASSERT_TRUE(args_b[args_list]);
 
     std::string argv_c[] = {"test", "--help"};
-    argh::argh args_c(argv_c);
+    argh::argh args_c(2, argv_c);
     ASSERT_TRUE(args_c[args_list]);
 
     std::string argv_d[] = {"test", "-hv"};
-    argh::argh args_d(argv_d);
+    argh::argh args_d(2, argv_d);
     ASSERT_TRUE(args_d[args_list]);
 
     std::string argv_e[] = {"test", "--help", "-v"};
-    argh::argh args_e(argv_e);
+    argh::argh args_e(3, argv_e);
     ASSERT_TRUE(args_e[args_list]);
 
     std::string argv_f[] = {"test", "-hv", "-o", "output.txt"};
-    argh::argh args_f(argv_f);
+    argh::argh args_f(4, argv_f);
     ASSERT_TRUE(args_f[args_list]);
 
     std::string argv_g[] = {"test", "-vo", "output.txt"};
-    argh::argh args_g(argv_g);
+    argh::argh args_g(3, argv_g);
     ASSERT_FALSE(args_g[args_list]);
 }
 
@@ -152,23 +155,23 @@ TEST(argh_argh_test, argh_argh_operator_flags_test)
 TEST(argh_argh_test, argh_argh_operator_param_test)
 {
     std::string argv_a[] = {"test"};
-    argh::argh args_a(argv_a);
+    argh::argh args_a(1, argv_a);
     ASSERT_STREQ("", args_a("-o").c_str());
 
     std::string argv_b[] = {"test", "-o", "output.txt"};
-    argh::argh args_b(argv_b);
+    argh::argh args_b(3, argv_b);
     ASSERT_STREQ("output.txt", args_b("-o").c_str());
 
     std::string argv_c[] = {"test", "-o", "output.txt", "-v"};
-    argh::argh args_c(argv_c);
+    argh::argh args_c(4, argv_c);
     ASSERT_STREQ("output.txt", args_c("-o").c_str());
 
     std::string argv_d[] = {"test", "-vo", "output.txt"};
-    argh::argh args_d(argv_d);
+    argh::argh args_d(3, argv_d);
     ASSERT_STREQ("output.txt", args_d("-o").c_str());
 
     std::string argv_e[] = {"test", "--output=output.txt"};
-    argh::argh args_e(argv_e);
+    argh::argh args_e(2, argv_e);
     ASSERT_STREQ("output.txt", args_e("--output").c_str());
 }
 
@@ -180,23 +183,23 @@ TEST(argh_argh_test, argh_argh_operator_param_list_test)
     std::string args_list[] = {"-o", "--output"};
 
     std::string argv_a[] = {"test"};
-    argh::argh args_a(argv_a);
+    argh::argh args_a(1, argv_a);
     ASSERT_STREQ("", args_a(args_list).c_str());
 
     std::string argv_b[] = {"test", "-o", "output.txt"};
-    argh::argh args_b(argv_b);
+    argh::argh args_b(3, argv_b);
     ASSERT_STREQ("output.txt", args_b(args_list).c_str());
 
     std::string argv_c[] = {"test", "--output", "output.txt", "-v"};
-    argh::argh args_c(argv_c);
+    argh::argh args_c(4, argv_c);
     ASSERT_STREQ("output.txt", args_c(args_list).c_str());
 
     std::string argv_d[] = {"test", "-vo", "output.txt"};
-    argh::argh args_d(argv_d);
+    argh::argh args_d(3, argv_d);
     ASSERT_STREQ("output.txt", args_d(args_list).c_str());
 
     std::string argv_e[] = {"test", "--output=output.txt"};
-    argh::argh args_e(argv_e);
+    argh::argh args_e(2, argv_e);
     ASSERT_STREQ("output.txt", args_e(args_list).c_str());
 }
 
@@ -206,7 +209,7 @@ TEST(argh_argh_test, argh_argh_operator_param_list_test)
 TEST(argh_argh_test, argh_argh_mark_param_test)
 {
     std::string argv[] = {"test", "-o", "output.txt"};
-    argh::argh args(argv);
+    argh::argh args(3, argv);
     args.mark_parameter("-o");
     args.mark_parameter("--nonexistent-parameter");
 }
@@ -216,37 +219,37 @@ TEST(argh_argh_test, argh_argh_mark_param_test)
 TEST(argh_argh_test, argh_argh_positional_simple_test)
 {
     std::string argv_a[] = {"test"};
-    argh::argh args_a(argv_a);
+    argh::argh args_a(1, argv_a);
     ASSERT_STREQ("test", args_a[0].c_str());
     ASSERT_STREQ("", args_a[1].c_str());
 
     std::string argv_b[] = {"test", "output.txt"};
-    argh::argh args_b(argv_b);
+    argh::argh args_b(2, argv_b);
     ASSERT_STREQ("test", args_b[0].c_str());
     ASSERT_STREQ("output.txt", args_b[1].c_str());
     ASSERT_STREQ("", args_b[2].c_str());
 
     std::string argv_c[] = {"test", "output.txt", "--verbose"};
-    argh::argh args_c(argv_c);
+    argh::argh args_c(3, argv_c);
     ASSERT_STREQ("test", args_c[0].c_str());
     ASSERT_STREQ("output.txt", args_c[1].c_str());
     ASSERT_STREQ("", args_c[2].c_str());
 
     std::string argv_d[] = {"test", "input.txt", "--verbose", "--output", "output.txt"};
-    argh::argh args_d(argv_d);
+    argh::argh args_d(5, argv_d);
     ASSERT_STREQ("test", args_d[0].c_str());
     ASSERT_STREQ("input.txt", args_d[1].c_str());
     ASSERT_STREQ("output.txt", args_d[2].c_str());
     ASSERT_STREQ("", args_d[3].c_str());
 
     std::string argv_e[] = {"test", "-vo", "output.txt", "input.txt"};
-    argh::argh args_e(argv_e);
+    argh::argh args_e(4, argv_e);
     ASSERT_STREQ("test", args_e[0].c_str());
     ASSERT_STREQ("output.txt", args_e[1].c_str());
     ASSERT_STREQ("input.txt", args_e[2].c_str());
 
     std::string argv_f[] = {"test", "--output=output.txt", "input.txt"};
-    argh::argh args_f(argv_f);
+    argh::argh args_f(3, argv_f);
     ASSERT_STREQ("test", args_f[0].c_str());
     ASSERT_STREQ("input.txt", args_f[1].c_str());
     ASSERT_STREQ("", args_f[2].c_str());
@@ -257,7 +260,7 @@ TEST(argh_argh_test, argh_argh_positional_simple_test)
 TEST(argh_argh_test, argh_argh_positional_complex_test)
 {
     std::string argv_a[] = {"test", "-o", "output.txt", "input.txt"};
-    argh::argh args_a(argv_a);
+    argh::argh args_a(4, argv_a);
     // Positional arguments should be "test", "output.txt", "input.txt".
     ASSERT_STREQ("test", args_a[0].c_str());
     ASSERT_STREQ("output.txt", args_a[1].c_str());
@@ -270,7 +273,7 @@ TEST(argh_argh_test, argh_argh_positional_complex_test)
     ASSERT_STREQ("", args_a[2].c_str());
 
     std::string argv_b[] = {"test", "-o", "output.txt", "input.txt", "--verbose"};
-    argh::argh args_b(argv_b);
+    argh::argh args_b(5, argv_b);
     // Positional arguments should be "test", "output.txt", "input.txt".
     ASSERT_STREQ("test", args_b[0].c_str());
     ASSERT_STREQ("output.txt", args_b[1].c_str());
@@ -283,7 +286,7 @@ TEST(argh_argh_test, argh_argh_positional_complex_test)
     ASSERT_STREQ("", args_b[2].c_str());
 
     std::string argv_c[] = {"test", "input.txt", "--output", "output.txt", "--verbose"};
-    argh::argh args_c(argv_c);
+    argh::argh args_c(5, argv_c);
     // Positional arguments should be "test", "input.txt", "output.txt".
     ASSERT_STREQ("test", args_c[0].c_str());
     ASSERT_STREQ("input.txt", args_c[1].c_str());
@@ -296,7 +299,7 @@ TEST(argh_argh_test, argh_argh_positional_complex_test)
     ASSERT_STREQ("", args_c[2].c_str());
 
     std::string argv_d[] = {"test", "--output=output.txt", "input.txt", "--verbose"};
-    argh::argh args_d(argv_d);
+    argh::argh args_d(4, argv_d);
     // Positional arguments should be "test", "input.txt".
     ASSERT_STREQ("test", args_d[0].c_str());
     ASSERT_STREQ("input.txt", args_d[1].c_str());
